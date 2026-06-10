c80c5bc52938af9a8b6f07c610d2e0b83a0b4e63
Afandi Hanif <afandihanif219@gmail.com>
2026-06-11 06:53:48 +0700

    fix js

--- script.js
+++ script.js
@@ -22,12 +22,9 @@ function revealSurprise() {
   const revealPage = document.getElementById("reveal-page");
   const overlay = document.getElementById("transition-overlay");
 
-  // 1. Jalankan efek memudar pada halaman terminal bawaanmu
-  terminalPage.classList.add("opacity-0");
-
   // 2. MUNCULKAN HUJAN KEPALA DEKU
   overlay.classList.remove("hidden");
-  const jumlahKepala = 40;
+  const jumlahKepala = 50;
 
   for (let j = 0; j < jumlahKepala; j++) {
     const deku = document.createElement("img");
@@ -41,10 +38,12 @@ function revealSurprise() {
     deku.style.top = `${posY}px`;
 
     // Delay acak biar estetik
-    deku.style.animationDelay = `${Math.random() * 0.5}s`;
+    deku.style.animationDelay = `${Math.random() * 0.2}s`;
 
     overlay.appendChild(deku);
   }
+
+  terminalPage.classList.add("opacity-0");
 
   // 3. PROSES PERGANTIAN HALAMAN (Sambil kepala Deku berterbangan)
   setTimeout(() => {
@@ -58,7 +57,7 @@ function revealSurprise() {
     setTimeout(() => {
       revealPage.classList.add("opacity-100");
     }, 50);
-  }, 1200); // Durasi transisi pas di tengah-tengah hujan Deku
+  }, 1000); // Durasi transisi pas di tengah-tengah hujan Deku
 }
 
 window.onload = typeWriter;
